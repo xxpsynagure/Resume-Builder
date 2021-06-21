cab8fdee379325a8b798fad9707298a6a06074b8
pravin-pk <justpk88@gmail.com>
2021-06-21 20:13:55 +0530

    finished commenting the introduction class

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ import java.awt.event.*;
 import java.awt.*;
 
 /*                                          INTRODUCTION FRAME CLASS                                            */
-class introclass {
-  public void introfunction() {
+class introclass { // class for the introduction JFrame
+  public void introfunction() { // a fuction to perform the introduction frame job
     JFrame introFrame = new JFrame("Introduction Page"); // Creating a JFrame for Itroduction page
     introFrame.setSize(500, 500); // setting frame size to 500x500 pixels
     introFrame.setLayout(null); // setting the layout to null so that we can give bounds to the components in the frame
@@ -28,41 +28,42 @@ class introclass {
     registerbtn.setFocusable(false); // this is to make the button to add a rectangle to itself when clicked
     introFrame.add(registerbtn); // adding the "sign up" button into the INTRODUCTION frame
 
-    ImageIcon img = new ImageIcon("leaflogo.jpg");
-    introFrame.setIconImage(img.getImage());
+    ImageIcon img = new ImageIcon("leaflogo.jpg"); // Creating a ImageIcon object to set an icon for the frame
+    introFrame.setIconImage(img.getImage()); // adding the naruto symbol as the icon for introduction frame
 
-    introFrame.getContentPane().setBackground(new Color(0, 0, 0));
-    JLabel leaf = new JLabel(new ImageIcon("leaflogo.jpg"));
-    leaf.setBounds(0, 20, 500, 500);
-    introFrame.add(leaf);
+    introFrame.getContentPane().setBackground(new Color(0, 0, 0)); // setting a black Background to the introduction Frame
+    JLabel leaf = new JLabel(new ImageIcon("leaflogo.jpg")); // setting the naruto sysbol as our background for frame
+    leaf.setBounds(0, 20, 500, 500); // setting its bounds
+    introFrame.add(leaf); // adding the background image into frame
 
-    loginbtn.addActionListener(new ActionListener() {
+    loginbtn.addActionListener(new ActionListener() { // action listener which is activated when the "sign in" button is clicked
 
       @Override
-      public void actionPerformed(ActionEvent e) {
-        introFrame.dispose();
-        loginclass logincall = new loginclass();
-        logincall.loginfunction();
+      public void actionPerformed(ActionEvent e) { // perform the following things when the "sign in" button is clicked
+        introFrame.dispose(); // closing the introduction frame
+        loginclass logincall = new loginclass(); // creating a object to call the login frame
+        logincall.loginfunction(); // calling a fuction which has the login form
         // JOptionPane.showMessageDialog(null, "Will Add soon!!", "Message box",
         // JOptionPane.INFORMATION_MESSAGE);
       }
     });
 
-    registerbtn.addActionListener(new ActionListener() {
+    registerbtn.addActionListener(new ActionListener() { // perform the following things when the "sign up" button is clicked
 
       @Override
-      public void actionPerformed(ActionEvent e) {
-        registerclass registercall = new registerclass();
-        introFrame.dispose();
+      public void actionPerformed(ActionEvent e) { // perform the following things when the "sign up" button is clicked
+        registerclass registercall = new registerclass(); // creating a object to call the register frame
+        introFrame.dispose(); // closing the introduction frame
         // JOptionPane.showMessageDialog(null, "Under Production", "Message box", JOptionPane.INFORMATION_MESSAGE);
-        registercall.registerfunction();
+        registercall.registerfunction(); // calling a fuction which has the register form
       }
     });
 
-    introFrame.setVisible(true);
+    introFrame.setVisible(true); // setVisible is set to "true" , so that the frame we created is displaced
   }
 
 }
+// END OF INTRODUCTION FRAME;
 // ---------------------------------------------------------------------------------------------------------------------------------
 // ================================================================================================================================+
 
