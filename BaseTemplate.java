656cb31499b111bc87d575f68977ac784a07b2aa
Mohammed Shehzad <m.shehzadsajid@gmail.com>
2021-07-10 06:23:00 +0530

    menuBar and Demo navBar added to abstract class
    -JMenuItem 'logout' leads back to the loginFrame
    -Demo navigation bar added

--- BaseTemplate.java
+++ BaseTemplate.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 
+import java.awt.Color;
+import java.awt.event.*;
+
 //base template of where the panes should be added
 
 
@@ -14,6 +17,30 @@ public abstract class BaseTemplate extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null);
         
+        //menuBar added to abstract class
+        JMenuBar menuBar = new JMenuBar();
+        JMenu settings = new JMenu("Settings");
+        JMenuItem logout = new JMenuItem("Log out");
+        settings.add(logout);
+        menuBar.add(settings);
+
+        //actionListener added to logout menuitem which leads back to the login page
+        logout.addActionListener(new ActionListener(){
+
+            public void actionPerformed(ActionEvent e){
+                LoginFrame login = new LoginFrame();
+                dispose();
+
+            }
+        });
+
+        setJMenuBar(menuBar);
+
+        //Demo navigation Bar
+        JPanel navBar = new JPanel();
+        navBar.setBackground(Color.white);
+        navBar.setBounds(0, 0, 75, 600);
+        add(navBar);
 
     }
 }
