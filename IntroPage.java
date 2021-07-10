bad688d7d8d58a75a2fc262c681c5f06600b854d
Pravin Kumar <justpk88@gmail.com>
2021-07-10 16:02:10 +0530

    changed names

--- IntroPage.java
+++ IntroPage.java
@@ -19,30 +19,30 @@ public class IntroPage extends BaseTemplate {
         welcome.setBounds(100, 100, 200, 25);
         intro.add(welcome);
         
-        JPanel button1 = new JPanel();
-        button1.setBackground(Color.white);
-        button1.setBounds(192, 206, 150, 150);
-        intro.add(button1);
+        JPanel panelBtn1 = new JPanel();
+        panelBtn1.setBackground(Color.white);
+        panelBtn1.setBounds(192, 206, 150, 150);
+        intro.add(panelBtn1);
         
-        JPanel button2 = new JPanel();
-        button2.setBackground(Color.white);
-        button2.setBounds(462, 206, 150, 150);
-        intro.add(button2);
+        JPanel panelBtn2 = new JPanel();
+        panelBtn2.setBackground(Color.white);
+        panelBtn2.setBounds(462, 206, 150, 150);
+        intro.add(panelBtn2);
         
-        JPanel button3 = new JPanel();
-        button3.setBackground(Color.white);
-        button3.setBounds(732, 206, 150, 150);
-        intro.add(button3);
+        JPanel panelBtn3 = new JPanel();
+        panelBtn3.setBackground(Color.white);
+        panelBtn3.setBounds(732, 206, 150, 150);
+        intro.add(panelBtn3);
         
-        JPanel button4 = new JPanel();
-        button4.setBackground(Color.white);
-        button4.setBounds(192, 406, 150, 150);
-        intro.add(button4);
+        JPanel panelBtn4 = new JPanel();
+        panelBtn4.setBackground(Color.white);
+        panelBtn4.setBounds(192, 406, 150, 150);
+        intro.add(panelBtn4);
         
-        JPanel button5 = new JPanel();
-        button5.setBackground(Color.white);
-        button5.setBounds(462, 406, 150, 150);
-        intro.add(button5);
+        JPanel panelBtn5 = new JPanel();
+        panelBtn5.setBackground(Color.white);
+        panelBtn5.setBounds(462, 406, 150, 150);
+        intro.add(panelBtn5);
         
         add(intro);
         pack();
